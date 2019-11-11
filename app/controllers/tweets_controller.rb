65e4688be260bdab0ff63e38f0a6d02c5569a4ce
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-11 16:43:14 +0900

    軽微な修正
    軽微な修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
 before_action :set_tweet, only: [:edit, :show]
-before_action :move_to_index, except: [:index, :show]
+# before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
