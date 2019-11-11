72b169c5a4756675491c81a3ec5fb91fb8fcb124
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-11 13:39:44 +0900

    テスト応用編
    FactoryBotの利用方法まで

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"abe"}
+    email                 {"kkk@kkk"}
+    password              {"0000000000"}
+    password_confirmation {"0000000000"}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
+
+    it "is valid with a nickname, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "is invalid without a nickname" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
+
+    it "is invalid without a email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
   end
 end
